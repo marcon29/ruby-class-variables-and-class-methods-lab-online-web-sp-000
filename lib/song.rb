1eb083a116f8c4b739a1c9a0516b0b54aebb5bdc
Mark Stabler <mark@markstabler.com>
2020-03-14 00:09:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,30 @@
 class Song
   attr_reader :name, :artist, :genre
 
+  @@count = 0      #total songs in class
+  @@artists = []   #list of all artists in class
+  @@genres = []    #list of all genres in class
+  
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
   end
+
+  #returns total songs in class
+def self.count
+  @@count
+end
+
+#returns list of all artists in class
+def self.artists
+  @@artists
+end
+
+#returns list of all genres in class
+def self.genres 
+  @@genres
+end
 
 
   # produces individual songs
